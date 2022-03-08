38c8dfa991b2e7a31d7774fabb9f90124c4876fd
Jan Uhlmann <uhlmann.jan@gmail.com>
2022-03-08 06:14:29 +0100

    small adaptions

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export default {
   parent: 'game',
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
+    width: 600,
     height: 600,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
